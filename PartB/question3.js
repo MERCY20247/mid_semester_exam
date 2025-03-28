c5f85ef13fd27b31d561392e375e464568c283a2
nasejje mariam lumala <aziirwemercy@gmail.com>
2025-03-28 10:27:14 +0300

    mid_sem_test

---
+++ PartB/question3.js
@@ -0,0 +1,4 @@
+// 3. Explain the difference between == and === in JavaScript
+//==: Compares values after type coercion.
+// While
+// ===:Compares both value and data type without doing type coercion.
